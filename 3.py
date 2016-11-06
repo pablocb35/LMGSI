0872c4f2bb332d040059dce6454b622a18d2d8cd
pablocb35 <pablo.couceborrego@gmail.com>
2016-11-06 13:48:35 +0100

    Basico 3
    Crea una lista de números enteros con datos introducidos por teclado. Tu
    programa debe sumarlos.

---
+++ 3.py
@@ -0,0 +1,12 @@
+#!/public_html/LM/B1Prog$ ipython 
+#-*- coding: utf-8 -*-
+
+raw_input( 'utiliza la función "sumar" para sumar los elementos de tu lista:  ')
+def sumar(lista):
+	resultado = 0
+	for elemento in lista:
+		resultado= resultado + elemento
+	return resultado
+
+
+
